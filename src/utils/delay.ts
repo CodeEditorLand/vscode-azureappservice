767c864d508ba2b9b29b54378cfca560671e8ded
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-09 19:17:36 +0300

    squash!

--- src/utils/delay.ts
+++
@@ -1,8 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-export async function delay(delayMs: number): Promise<void> {
-    await new Promise<void>((resolve: () => void): void => { setTimeout(resolve, delayMs); });
-}
